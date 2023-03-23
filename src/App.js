7ccbbe174a0f02e8071179960e0d71093d70812c
treva556 <marktreva92@gmail.com>
2023-03-24 00:33:44 +0300

    app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, { useState } from 'react';
-
-const initialTodos = [
-  { id: 1, text: 'Buy groceries', completed: false },
-  { id: 2, text: 'Walk the dog', completed: false },
-  { id: 3, text: 'Do laundry', completed: true }
-];
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
-
   return (
-    <div>
-      <h1>Todo List</h1>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.text} - {todo.completed ? 'Completed' : 'Incomplete'}
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <TodoWrapper  />
     </div>
   );
 }
